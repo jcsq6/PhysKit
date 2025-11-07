2b9f89f4c81fc4870478dcd89bea58d8696c00f3
JC Squires <jcsquires22@gmail.com>
2025-11-07 16:46:17 -0500

    Fix convert for gcc

--- tests/include/convert.h
+++ tests/include/convert.h
@@ -10,24 +10,25 @@ namespace graphics
 {
 namespace detail
 {
-template <std::size_t... Is> consteval auto index_array(std::index_sequence<Is...> /*seq*/)
+template <typename Ret, std::size_t... Indices>
+constexpr auto expand(auto &&nth, std::index_sequence<Indices...> /*helper*/)
 {
-    return std::array<std::size_t, sizeof...(Is)>{Is...};
+    return Ret{nth(Indices)...};
 }
 } // namespace detail
 template <typename T, int Size, mp_units::Quantity Q>
 constexpr Magnum::Math::Vector<Size, T> to_magnum_vector(const physkit::unit_mat<Q, Size, 1> &v)
 {
-    auto [... indices] = detail::index_array(std::make_index_sequence<Size>{});
-    return Magnum::Math::Vector<Size, T>{
-        static_cast<T>(v[indices].numerical_value_in(Q::reference))...};
+    return detail::expand<Magnum::Math::Vector<Size, T>>([&](auto i)
+                          { return static_cast<T>(v[i].numerical_value_in(Q::reference)); },
+                          std::make_index_sequence<Size>{});
 }
 
 template <auto ref, typename T, typename U, std::size_t Size>
 constexpr physkit::vec<Size, ref, T> to_physkit_vector(const Magnum::Math::Vector<Size, U> &v)
 {
-    auto [... indices] = detail::index_array(std::make_index_sequence<Size>{});
-    return physkit::vec<Size, ref, T>(physkit::value_cast<ref, T>(v[indices] * ref)...);
+    return detail::expand<physkit::vec<Size, ref, T>>([&](auto i) { return physkit::value_cast<ref, T>(v[i] * ref); },
+                          std::make_index_sequence<Size>{});
 }
 
 Magnum::GL::Mesh to_magnum_mesh(const physkit::mesh &magnum_mesh) // TODO
